7a3f88af604f4bfa2209b99c983581d76a7b0d27
saelyang01 <123331887+saelyang01@users.noreply.github.com>
2023-10-11 14:54:55 -0400

    Add files via upload

---
+++ Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CSPproject
+{
+    public class Card
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Attack { get; set; }
+        public string Defense { get; set; }
+    }
+}
+
+

56498dfe0b17ed69f4c673de0209fa0de202d7e2
ShirleyYang <122475617+shirleyYang8@users.noreply.github.com>
2023-10-24 18:50:26 -0400

    Add files via upload

---
+++ TableSlot.cs
@@ -0,0 +1,42 @@
+namespace TableSlot;
+
+public class TableSlot
+{
+    // Properties to store information about the slot
+    public bool IsOccupied { get; set; }
+    public GamePiece OccupyingPiece { get; set; }
+
+    // Constructor
+    public TableSlot()
+    {
+        IsOccupied = false;
+        OccupyingPiece = null;
+    }
+
+    // Method to place a game piece into the slot
+    public void PlacePiece(GamePiece piece)
+    {
+        IsOccupied = true;
+        OccupyingPiece = piece;
+    }
+
+    // Method to remove the game piece from the slot
+    public void RemovePiece()
+    {
+        IsOccupied = false;
+        OccupyingPiece = null;
+    }
+}
+
+public class GamePiece
+{
+    // Properties to store information about the game piece
+    public string Name { get; set; }
+    // Add more properties as needed
+
+    // Constructor
+    public GamePiece(string name)
+    {
+        Name = name;
+    }
+}
---
+++ TableSlotMain.cs
@@ -0,0 +1,42 @@
+﻿namespace TableSlot;
+using System;
+
+class TableSlotMain
+{
+    static void Main()
+    {
+        // Create some game pieces
+        GamePiece piece1 = new GamePiece("Piece 1");
+        GamePiece piece2 = new GamePiece("Piece 2");
+
+        // Create table slots
+        TableSlot slot1 = new TableSlot();
+        TableSlot slot2 = new TableSlot();
+
+        // Place game pieces into slots
+        slot1.PlacePiece(piece1);
+        slot2.PlacePiece(piece2);
+
+        // Check if slots are occupied
+        Console.WriteLine($"Slot 1 occupied: {slot1.IsOccupied}");
+        Console.WriteLine($"Slot 2 occupied: {slot2.IsOccupied}");
+
+        // Get the names of occupying pieces
+        if (slot1.IsOccupied)
+        {
+            Console.WriteLine($"Slot 1 contains {slot1.OccupyingPiece.Name}");
+        }
+
+        if (slot2.IsOccupied)
+        {
+            Console.WriteLine($"Slot 2 contains {slot2.OccupyingPiece.Name}");
+        }
+
+        // Remove pieces from slots
+        slot1.RemovePiece();
+        slot2.RemovePiece();
+
+        Console.WriteLine($"Slot 1 occupied: {slot1.IsOccupied}");
+        Console.WriteLine($"Slot 2 occupied: {slot2.IsOccupied}");
+    }
+}

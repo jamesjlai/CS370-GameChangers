72889ffcb95c3c15736d6a6b738ade350e1967ae
jamesjlai <james.lai@emory.edu>
2023-10-29 20:04:40 -0400

    updated methods, wip

--- SOULS/Assets/Scripts/makeDeck.cs
+++ SOULS/Assets/Scripts/makeDeck.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
+using Newtonsoft.Json;
 
 public class Card : MonoBehaviour { // card class for card variables
     public string CardName { get; set; }
@@ -16,48 +18,37 @@ public class Card : MonoBehaviour { // card class for card variables
         return $"{CardName} - {Description} (Attack: {Attack}, Health: {Health}, Skill: {Skill ?? "None"}, Texture: {Texture}, BorderColor: {BorderColor ?? "None"}, Probability: {Probability})";
     }
 }
+//public class CardsContainer
+//{
+//    List<Card> cp { get; set; }
+//}
 
 public class makeDeck : MonoBehaviour{ // make deck class, instatiate for decks and card_pool
     
     
     public Dictionary<string, object> globalVariables = new Dictionary<string, object>();
-    public static readonly Random random = new Random();  // Static instance of Random
+    public List<Card> card_pool;
     
     public void GameStart(){
         DeserializeCards();
-        globalVariables.add("deck1", CreateRandomDeck(card_pool, 25));
-        globalVariables.add("deck2", CreateRandomDeck(card_pool, 25));
-        globalVariables.add("hand1", new List<Card>());
-        globalVariables.add("hand2", new List<Card>());
-        Draw(hand1, deck1, 4);
-        Draw(hand2, deck2, 4);
+        //globalVariables.Add("deck1", CreateRandomDeck(card_pool, 25));
+        //globalVariables.Add("deck2", CreateRandomDeck(card_pool, 25));
+        //globalVariables.Add("hand1", new List<Card>());
+        //globalVariables.Add("hand2", new List<Card>());
+        //Draw(hand1, deck1, 4);
+        //Draw(hand2, deck2, 4);
     }
 
-    public void List<Card> DeserializeCards()
+    public void DeserializeCards()
     {
-        string jsonFilePath = "Cards.json"; // Assuming the filename is "Cards.json"
         string path = AppDomain.CurrentDomain.BaseDirectory + "Cards.json";
         string jsonData = File.ReadAllText(path);
-        this.card_pool = JsonConvert.DeserializeObject<CardsContainer>(jsonData);
-    }
-
-    public void Card CopyCard(Card original)
-    {
-        return new Card
-        {
-            CardName = original.CardName,
-            Description = original.Description,
-            Attack = original.Attack,
-            Health = original.Health,
-            Skill = original.Skill,
-            Texture = original.Texture,
-            BorderColor = original.BorderColor,
-            Probability = original.Probability
-        }
+        this.card_pool = JsonConvert.DeserializeObject<List<Card>>(jsonData);
     }
 
     public static Card DrawRandom(List<Card> pool)
     {
+        Random random = new();
         int randomIndex = random.Next(pool.Count);
         Card originalCard = pool[randomIndex];
         return originalCard;
@@ -78,7 +69,7 @@ public class makeDeck : MonoBehaviour{ // make deck class, instatiate for decks 
     public void Draw(string hand, string deck, int draw_amount){
         if (globalVariables[deck].Peek() == null) throw new InvalidOperationException("Deck Empty");;
         while(deck.Peek() != null && draw_amount > 0) {
-            globalVariables[hand].add(globalVariables[deck].Pop());
+            globalVariables[hand].Add(globalVariables[deck].Pop());
             draw_amount--;
         }
     }
